1789cd497d932268f882f3f56c06f757f6d776c2
alifaragmahmed <alifaragmahmed@gmail.com>
2021-04-12 14:45:57 +0200

    Edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {FeatureComponent} from './components/feature/feature.component';
 import {ContactComponent} from './components/contactus/contact.component';
 import {DocComponent} from './components/doc/doc.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: DocComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'features', component: FeatureComponent},
   {path: 'contact', component: ContactComponent}
 ];
